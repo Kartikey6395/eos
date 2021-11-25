36767d8465b332f63cbf9dc04b18acb44eac4bbc
Kartikey6395 <kartikeytripathi6395@gmail.com>
2021-11-25 17:26:33 +0530

    implemented sub function

--- arith/main.c
+++ arith/main.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 int main(int argc,char const *argv[])
 {
-    int result=0;
-    result =add(20,60);
-    printf("Addition=%d\n",result);
+    int Addition=0,subtraction=0;
+    Addition =add(20,60);
+    printf("Addition=%d\n",Addition);
+    subtraction =sub(20,65);
+    printf("Subtraction=%d\n",subtraction);
     return 0;
 }
